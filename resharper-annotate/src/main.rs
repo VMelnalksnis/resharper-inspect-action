7cf9114f6f9b9f65187058c936dba5f0e9a93b23
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2022-08-19 21:09:12 +0300

    Add treat-warnings-as-errors parameter

--- resharper-annotate/src/main.rs
+++ resharper-annotate/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
+use std::str::FromStr;
 use crate::report::parse_report;
 
 mod report;
 
 fn main() {
-    let report_filepath = env::args().nth(1).expect("no report filepath specified");
+    let mut args = env::args();
+    let report_filepath = args.nth(1).expect("no report filepath specified");
+    let treat_warnings_as_errors = args.nth(0).map(|s| bool::from_str(s.as_str()).unwrap()).unwrap();
+
     let file_content: String = std::fs::read_to_string(report_filepath).unwrap();
     let report = parse_report(&file_content);
 
@@ -20,7 +24,8 @@ fn main() {
         .filter(|i| warning_type_ids.contains(&&i.type_id));
 
     for warning in warnings {
-        write_annotation("warning", &warning.file, &warning.line, &warning.type_id, &warning.message);
+        let level = if treat_warnings_as_errors { "error" } else { "warning" };
+        write_annotation(level, &warning.file, &warning.line, &warning.type_id, &warning.message);
     }
 
     let error_type_ids: Vec<&String> = report.issue_types.issue_types
@@ -38,7 +43,7 @@ fn main() {
         write_annotation("error", &error.file, &error.line, &error.type_id, &error.message);
     }
 
-    if !error_type_ids.is_empty() {
+    if !error_type_ids.is_empty() || (treat_warnings_as_errors && !warning_type_ids.is_empty()) {
         std::process::exit(1)
     }
 }
